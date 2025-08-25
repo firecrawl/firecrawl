6b1d37a5e4ee1e3c493c446ffbd42f9cf3373a5f
amplitudes <hey@amplitudes.me>
2025-08-26 00:11:21 +0100

    fix(playwright-service): content-type parsing

--- apps/playwright-service-ts/api.ts
+++ apps/playwright-service-ts/api.ts
@@ -142,7 +142,7 @@ const scrapePage = async (page: Page, url: string, waitUntil: 'load' | 'networki
   if (response) {
     headers = await response.allHeaders();
     ct = Object.entries(headers).find(x => x[0].toLowerCase() === "content-type")?.[1];
-    if (ct && (ct[1].includes("application/json") || ct[1].includes("text/plain"))) {
+    if (ct && (ct.includes("application/json") || ct.includes("text/plain"))) {
       content = (await response.body()).toString("utf8"); // TODO: determine real encoding
     }
   }
