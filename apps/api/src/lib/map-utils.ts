381da9b0c120425ac8ba52c466f56ea49d002afb
Gaurav Chadha <gauravchadha1676@gmail.com>
2025-10-13 12:49:00 +0530

    preserve-smart-map-comment
    Signed-off-by: Gaurav Chadha <gauravchadha1676@gmail.com>

--- apps/api/src/lib/map-utils.ts
+++ apps/api/src/lib/map-utils.ts
@@ -22,6 +22,7 @@ import { performCosineSimilarityV2 } from "./map-cosine";
 import { Logger } from "winston";
 import { logger } from "./logger";
 
+// Max Links that "Smart /map" can return
 const MAX_FIRE_ENGINE_RESULTS = 500;
 const MAX_MAP_LIMIT = 1000;
 
